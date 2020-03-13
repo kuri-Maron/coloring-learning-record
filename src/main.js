0384a868af1276037dd0c16bc047acbccf67e04d
Shun-Maron <kurisyun4321@gmail.com>
2020-03-13 21:37:01 +0900

    firebaseのライブラリインポートと自分のPJと紐付け初期化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJv9rgteA7J72JsDnxmJmlg7BDMIPgWIg",
+  authDomain: "color-record-test.firebaseapp.com",
+  databaseURL: "https://color-record-test.firebaseio.com",
+  projectId: "color-record-test",
+  storageBucket: "color-record-test.appspot.com",
+  messagingSenderId: "1047522148283",
+  appId: "1:1047522148283:web:2861562b5a158cec2cce9e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
