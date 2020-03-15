c8a51c4dba2918411f47affa5251a3c29864e50f
Shun-Maron <kurisyun4321@gmail.com>
2020-03-15 09:18:28 +0900

    add: plugins (vuetify) 導入完了

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import firebase from "firebase";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
@@ -21,5 +22,6 @@ firebase.initializeApp(firebaseConfig);
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
