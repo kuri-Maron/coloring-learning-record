7b1219973938c437197b4eda1a4f33ded8681e56
Shun-Maron <kurisyun4321@gmail.com>
2020-03-18 20:30:16 +0900

    v-ifでレンダリングを制御。あと、console.logデバッグ。

--- src/components/Coloring.vue
+++ src/components/Coloring.vue
@@ -34,85 +34,88 @@ export default {
     }
     //   コンポーネント生成時にuserが認証済かどうか判断したい。
     console.log("ユーザ認証状況は：", this.$store.state.user);
+    // console.log("created");
     this.featchRecords();
   },
   mounted() {
-    this.$store.watch(
-      (state, getters) => getters.user,
+      this.$store.watch(
+          (state, getters) => getters.user,
       (newValue, oldValue) => {
-        console.log("user changed! %s => %s", oldValue, newValue);
+          console.log("user changed! %s => %s", oldValue, newValue);
         this.featchRecords();
+    console.log("mounted");
       }
     );
   },
   computed: {
-    activeCellObj() {
-      return {
-        isActive: true,
+      activeCellObj() {
+          return {
+              isActive: true,
         task: "work",
         colorCode: this.colorCode
       };
     }
   },
   methods: {
-    selectCell(index) {
-      if (this.$store.state.user) {
-        // 対象セルがアクティブの場合
+      selectCell(index) {
+          if (this.$store.state.user) {
+              // 対象セルがアクティブの場合
         if (
-          this.cellLists[index].isActive === true &&
+            this.cellLists[index].isActive === true &&
           this.cellLists[index].id !== null
         ) {
-          //   そのセルを削除して、配列の最後尾に非アクティブなセルを追加する
+            //   そのセルを削除して、配列の最後尾に非アクティブなセルを追加する
           firebase
             .firestore()
             .collection(`users/${this.$store.getters.uid}/records`)
             .doc(this.cellLists[index].id)
             .delete()
             .then(() => {
-              this.cellLists.splice(index, 1);
+                this.cellLists.splice(index, 1);
               this.cellLists.push(this.notActiveObj);
             });
           // 対象セルが非アクティブの場合
         } else if (index === 0 || this.cellLists[index - 1].isActive === true) {
-          //   対象セルをアクティブにするTODO: 今後はタイムスタンプとかドキュメントID も格納するかも
+            //   対象セルをアクティブにするTODO: 今後はタイムスタンプとかドキュメントID も格納するかも
           firebase
             .firestore()
             .collection(`users/${this.$store.getters.uid}/records`)
             .add({
-              ...this.activeCellObj,
+                ...this.activeCellObj,
               timeStamp: firebase.firestore.FieldValue.serverTimestamp()
             })
             // .add({ isActive: true, task: "work" })
             .then(doc => {
-              this.$set(this.cellLists, index, {
-                ...this.activeCellObj,
+                this.$set(this.cellLists, index, {
+                    ...this.activeCellObj,
                 id: doc.id
               });
             });
         }
       } else {
-        alert("ログインしてくだい！");
+          alert("ログインしてくだい！");
       }
     },
     // 認証チェックメソッド
     checkUserState() {
-      console.log("ボタン押下::ユーザ認証状況は：", this.$store.state.user);
+        console.log("ボタン押下::ユーザ認証状況は：", this.$store.state.user);
     },
     checkAnythig() {
-      this.cellLists[0].task.get().then(doc => console.log(doc.data()));
+        this.cellLists[0].task.get().then(doc => console.log(doc.data()));
     },
     featchRecords() {
+        console.log("featchRecords");
       if (this.$store.state.user) {
-        //   TODO: 取得する時、タイムスタンプの昇順で取得する必要がある。
+          //   TODO: 取得する時、タイムスタンプの昇順で取得する必要がある。
         firebase
           .firestore()
           .collection(`users/${this.$store.getters.uid}/records`)
           .orderBy("timeStamp")
           .get()
           .then(snapshot => {
-            snapshot.docs.forEach((doc, index) => {
-              this.$set(this.cellLists, index, {
-                ...doc.data(),
+              snapshot.docs.forEach((doc, index) => {
+                  this.$set(this.cellLists, index, {
+                      ...doc.data(),
                 id: doc.id
                 //   id: doc.id,
                 //   cell: doc.data()
@@ -121,7 +124,7 @@ export default {
             });
           });
       } else {
-        console.log("未ログインのためデータ取得する不可");
+          console.log("未ログインのためデータ取得する不可");
       }
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <Coloring/>
+    <!-- <Coloring/> -->
+    <Coloring v-if="$store.state.user"/>
   </div>
 </template>
 
