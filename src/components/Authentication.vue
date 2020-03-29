b90a7c1e8d310c9c2c3424b51395cd1ba0466302
Shun-Maron <kurisyun4321@gmail.com>
2020-03-29 19:00:05 +0900

    fix: 認証コンポーネントのレイアウト調整。

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="authentication">
+  <div class="d-flex align-center">
     <!-- ログイン時のレイアウト -->
-    <div v-if="user !== null" key="login">
-      <p>{{user.displayName}}</p>
-      <v-btn type="button" @click="logout()">Sign out</v-btn>
+    <div class="d-flex align-center" v-if="user !== null" key="login">
+      <v-avatar max-height="100%">
+        <v-img class="shrink mr-2" contain max-width="50px" width="5vw" :src="user.photoURL"></v-img>
+      </v-avatar>
+      <span class="d-inline-block ma-3">{{user.displayName}}</span>
+      <v-btn class="ma-2" outlined type="button" @click="logout()">Sign out</v-btn>
       <!-- ログアウト時のレイアウト -->
     </div>
     <div v-else key="logout">
@@ -16,25 +19,27 @@
 import firebase from "firebase";
 import { mapActions } from "vuex";
 export default {
-    name: "Authentication",
-    created(){
-        // ログイン状態の取得（TODO: App.vueに写して試す。マウントの関係で正常に実行できない）
-        // firebase.auth().onAuthStateChanged(user => {
-        //     this.setCurrentUser(user);
-        // });
-    },
-    computed: {
-        user() {return this.$store.state.user}
-    },
-    methods: {
-        login() {
-            const provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithRedirect(provider);
-        },
-        logout() {
-            firebase.auth().signOut();
-        },
-        ...mapActions(["setCurrentUser"])
+  name: "Authentication",
+  created() {
+    // ログイン状態の取得（TODO: App.vueに写して試す。マウントの関係で正常に実行できない）
+    // firebase.auth().onAuthStateChanged(user => {
+    //     this.setCurrentUser(user);
+    // });
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
     }
-}
+  },
+  methods: {
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    },
+    logout() {
+      firebase.auth().signOut();
+    },
+    ...mapActions(["setCurrentUser"])
+  }
+};
 </script>
