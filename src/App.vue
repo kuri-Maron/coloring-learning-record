318e048462e0ce77985f1f34c6b7b6e0672efa88
Shun-Maron <kurisyun4321@gmail.com>
2020-03-31 21:55:08 +0900

    Improve: 無駄の削除、コメントの追加

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,23 @@
   <v-app>
     <v-app-bar app>
       <div class="d-flex align-center">
-        <!-- <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />-->
+        <!-- TODO: この丸出しのメニューをvuetifyのコンポーネント使用してレイアウト調整する -->
         <router-link class="linkFont" to="/">Home</router-link>|
         <router-link class="linkFont" to="/about">About</router-link>
       </div>
-
       <v-spacer></v-spacer>
-
-      <!-- 認証機能配置 -->
+      <!-- 認証機能コンポーネント -->
       <Authentication />
     </v-app-bar>
 
     <v-content>
+      <!-- コンテナーを横に広げる -->
       <v-container fluid>
         <router-view />
       </v-container>
     </v-content>
-      <select-color v-if="this.$route.meta.bottom"/>
+    <!-- metaにbottomが設定されているviewだけボトムバーを表示 -->
+    <select-color v-if="this.$route.meta.bottom" />
   </v-app>
 </template>
 
@@ -43,11 +36,13 @@ export default {
     SelectColor
   },
   created() {
+    // 認証状態の判別
     firebase.auth().onAuthStateChanged(user => {
       this.setCurrentUser(user);
     });
   },
   methods: {
+    // スプレット構文 or 分割代入 :挙動をあまり詳しく把握できていない(現状は一つなので、展開する必要もない・・)
     ...mapActions(["setCurrentUser"])
   }
 };
