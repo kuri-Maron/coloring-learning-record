e7c636b26401b25eace07d09331e1ea47cdc547b
Shun-Maron <kurisyun4321@gmail.com>
2020-04-09 20:34:12 +0900

    add: べき等を考慮していない総数カウント処理実装（修正必須）

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,20 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.countUp = functions
+  .region("asia-northeast1")
+  .firestore.document("users/{user}/records/{record}")
+  .onCreate((change, context) => {
+    const FieldValue = admin.firestore.FieldValue;
+    const userPath = context.params.user;
+
+    return db
+      .doc(`users/${userPath}`)
+      .update("docCount", FieldValue.increment(1));
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
