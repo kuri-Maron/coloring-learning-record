a89c9f7e6ac7d72e186983d4cf17c7127fb94645
Shun-Maron <kurisyun4321@gmail.com>
2020-04-11 14:24:10 +0900

    総合計の集計ロジック実装（冪等生に未対応

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,44 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const db = admin.firestore();
-
+// TODO: べき等を考慮して再実装。（frontで直でカウントしていく。＠task事にカウント分散。
 exports.countUp = functions
   .region("asia-northeast1")
   .firestore.document("users/{user}/records/{record}")
   .onCreate((change, context) => {
     const FieldValue = admin.firestore.FieldValue;
     const userPath = context.params.user;
+    const countTarget = change.data().colorCode; 
 
     return db
       .doc(`users/${userPath}`)
-      .update("docCount", FieldValue.increment(1));
+      .set({[`count_${countTarget}`]: FieldValue.increment(1)},{merge: true});
+    //   .update("docCount", FieldValue.increment(1));
   });
+
+//   TODO: マイナス値のバリデーション実装が分からない。。。＠getとして値判断してみたいなことはできるかもしれんが、readコストが。。
+  exports.countDown = functions
+  .region("asia-northeast1")
+  .firestore.document("users/{user}/records/{record}")
+  .onDelete((change, context) => {
+    const FieldValue = admin.firestore.FieldValue;
+    const userPath = context.params.user;
+    const countTarget = change.data().colorCode;
+
+    return db
+      .doc(`users/${userPath}`)
+      .set({[`count_${countTarget}`]: FieldValue.increment(-1)},{merge: true});
+    //   .update("docCount", FieldValue.increment(1));
+  });  
+
+//   トリガーされる順番が不確実だし、そもそもいきなりサブコレクション作ったとき発火しないっぽい
+// exports.initUserInformation = functions
+//   .region("asia-northeast1")
+//   .firestore.document("users/{user}")
+//   .onCreate((change, context) => {
+//     const userPath = context.params.user;
+//     return db.doc(`users/${userPath}`).set({ docCount: 0 });
+//   });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
