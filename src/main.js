4b548a3b910a8d73955e6027f687d3b1f8b27e32
kuri-Maron <kurisyun4321@gmail.com>
2021-07-11 15:39:38 +0900

    firebasse情報を外だし

--- src/main.js
+++ src/main.js
@@ -4,19 +4,10 @@ import router from "./router";
 import store from "./store";
 import firebase from "firebase";
 import vuetify from './plugins/vuetify';
+import firebaseConfig from "./firebase-config";
 
 Vue.config.productionTip = false;
 
-// firebaseのコンフィグ TODO:今後はhostingURLを利用したい
-const firebaseConfig = {
-  apiKey: "AIzaSyCJv9rgteA7J72JsDnxmJmlg7BDMIPgWIg",
-  authDomain: "color-record-test.firebaseapp.com",
-  databaseURL: "https://color-record-test.firebaseio.com",
-  projectId: "color-record-test",
-  storageBucket: "color-record-test.appspot.com",
-  messagingSenderId: "1047522148283",
-  appId: "1:1047522148283:web:2861562b5a158cec2cce9e"
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
